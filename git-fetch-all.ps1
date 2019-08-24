30fa7958c7c539b93e94b480a401e0b521fe417b
Shaun Sutterfield <dotnetsparky@gmail.com>
2019-08-24 08:40:32 -0700

    Add PowerShell variants of scripts.

---
+++ git-fetch-all.ps1
@@ -0,0 +1,23 @@
+# store the current dir
+$rootDir = Get-Location
+
+# Let the person running the script know what's going on.
+Write-Host "Fetching latest changes for all repositories..."
+
+# Find all git repositories and update it to the master latest revision
+foreach ($i in (Get-ChildItem -Filter ".git" -Directory -Hidden -Recurse))
+{
+    $path = [System.IO.Path]::GetDirectoryName($i.FullName)
+    Write-Host ""
+    Write-Host $path
+
+    # We have to go to the .git parent directory to call the pull command
+    Set-Location $path
+
+    git fetch --all --tags --prune --prune-tags --recurse-submodules
+
+    # lets get back to the CUR_DIR
+    Set-Location $rootDir
+}
+
+Write-Host "Complete!"
