5fcc47f3f3e897c6cd01256e1712287455f1a244
Shaun Sutterfield <ssutterfield@bhhc.com>
2019-09-04 06:56:44 -0700

    Add retries to fetch scripts, add clean script.

--- git-fetch-all.ps1
+++ git-fetch-all.ps1
@@ -1,3 +1,8 @@
+$maxRetries = 5
+$failedCount = 0
+$retryCount = 0
+$successCount = 0
+
 # store the current dir
 $rootDir = Get-Location
 
@@ -14,10 +19,30 @@ foreach ($i in (Get-ChildItem -Filter ".git" -Directory -Hidden -Recurse))
     # We have to go to the .git parent directory to call the pull command
     Set-Location $path
 
-    git fetch --all --tags --prune --prune-tags --recurse-submodules
+    $n = 0
+    while ($n -lt $maxRetries)
+    {
+        git fetch --all --tags --prune --prune-tags --recurse-submodules
+        if ($?)
+        {
+            $successCount += 1
+            break
+        }
+        $n += 1
+        $retryCount += 1
+        if ($n -ge $maxRetries) { $failedCount += 1 }
+    }
 
     # lets get back to the CUR_DIR
     Set-Location $rootDir
 }
 
 Write-Host "Complete!"
+if ( $retryCount -gt 0 ) { Write-Host "Retries: $retryCount" }
+if ( $failedCount -gt 0 )
+{
+    Write-Host "Failed: $failedCount"
+    exit 1
+}
+
+exit 0
